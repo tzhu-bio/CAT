8cb0f9c543396acf4d04756a8f1f486c61a8a36d
Tao Zhu <54537420+tzhu-bio@users.noreply.github.com>
2023-08-14 09:09:42 +0800

    Update MultiSampleDiffAnalysis.R

--- R/MultiSampleDiffAnalysis.R
+++ R/MultiSampleDiffAnalysis.R
@@ -213,7 +213,7 @@ plotClusterSpecificPeak <- function(specific_peak, zscore_min = -2, zscore_max =
   group <- data.frame(C=cutree(row_dend, k = as.integer(cluster_N)))
   group$Cluster <- paste0("Cluster",group$C)
   group <- group[order(group$Cluster),]
-  group$Cluster <- factor(group$Cluster, levels = sort(unique(group$Cluster)))
+  group$Cluster <- factor(group$Cluster, levels = gtools::mixedsort(unique(group$Cluster)))
   mat <- df1[rownames(group),]
   group <- subset(group,select=-(C))
   annotation <- mat[match(rownames(group),rownames(as.data.frame(mat))),]
